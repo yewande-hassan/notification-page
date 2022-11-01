aca4fa8a8b370aedcbc46612e833ba66e9a06dc4
Chigozie <ccasadu15@gmail.com>
2022-11-01 18:33:50 +0100

    Correcions of notification page code

---
+++ src/Components/Data/data.js
@@ -0,0 +1,69 @@
+import image1 from "../../images/avatar-mark-webber.webp"
+import image2 from "../../images/avatar-angela-gray.webp";
+import image3 from "../../images/avatar-jacob-thompson.webp";
+import image4 from "../../images/avatar-rizky-hasanuddin.webp";
+import image5 from "../../images/avatar-kimberly-smith.webp";
+import image6 from "../../images/avatar-nathan-peterson.webp";
+import image7 from "../../images/avatar-anna-kim.webp";
+export const data = [
+    {
+        id:1,
+        name:"Mark Webber ",
+        message:"reacted to your recent post",
+        activity: " My first tournament today!",
+        time:"1 min ago",
+        image: image1,
+        isRead:false
+    },
+    {
+        id:2,
+        name:"Angela Gray ",
+        message:"followed you",
+        time:"5 mins ago",
+        image: image2,
+        isRead:false
+    },
+    {
+        id:3,
+        name:"Jacob Thompson ",
+        message:"has joined your group",
+        group:" Chess Group",
+        time:"1 day ago",
+        image: image3,
+        isRead:false
+    },
+    {
+        id:4,
+        name:"Rizky Hasanuddin ",
+        message:"sent a private message",
+        time:"5 days ago",
+        image: image4,
+        isRead:true
+    },
+    {
+        id:5,
+        name:"Kimberly Smith ",
+        message:"commented on your picture",
+        time:"1 week ago",
+        image: image5,
+        isRead:true
+    },
+    {
+        id:6,
+        name:"Nathan Peterson ",
+        message:"reacted to your recent post",
+        activity:" 5 end-game stategies to increase your win rate",
+        time:"2 weeks ago",
+        image: image6,
+        isRead:true
+    },
+    {
+        id:7,
+        name:"Anna Kim ",
+        message:"left the group",
+        group:" Chess Group",
+        time:"2 weeks ago",
+        image: image7,
+        isRead:true
+    },
+]
--- src/Components/Notification/Notification.js
+++ src/Components/Notification/Notification.js
@@ -1,14 +1,17 @@
 import React from "react";
-import "./Notification.css"
+import "./Notification.css";
+
 
 export default function Notification ({images, name, message, activity, time, group}){
+ 
     return(
         <div className="container">
             <img className="images" src={images}/>
             <div>
-                <p><span className="name">{name}</span><span className="message">{message}</span><span className="activity">{activity}</span><span className="group">{group}</span></p>
+                <p><span className="name">{name}</span><span className="message">{message}</span><span className="activiti">{activity}</span><span className="group">{group}</span></p>
                 <p className="time">{time}</p>
             </div>
-        </div>
+        </div> 
+  
     )
 }
