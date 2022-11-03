ba5935d8d58f0a7b6351e0d554ba43ac3df277fa
Chigozie <ccasadu15@gmail.com>
2022-11-03 14:58:03 +0100

    Functionalities on Notification Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import Notification from './Components/Notification/Notification';
-import {data} from "../src/Components/Data/data"
+import { data } from "../src/Components/Data/data"
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [read, isRead] = useState(false);
-  const handleClick= ()=>{
-      isRead(true)
+  const [item, isItem] = useState(data);
+  const handleClick= () =>{
+      const newData = 
+      data.map((datas)=>
+      datas.isRead !== true ?
+        {...datas, isRead:true } : datas
+      )
+     
+      isItem(newData)
   }
+  const markAsRead = (id) =>{
+    const readArray = item.map((datas)=>
+    // console.log(datas.id == id+1)
+    (datas.id === id+1)?
+      {...datas, isRead:true} : datas
+
+  )
+  console.log(data.name)
+  isItem(readArray);
+}
   return (
     
     <div className="App">
@@ -15,9 +31,9 @@ function App() {
         <p className='heading'>Notifications</p>
         <p className="activity" onClick={handleClick}>Mark all as read</p>
       </div>
-      {data.map((Data, index)=>{
-        return <Notification images={Data.image} key={index} name={Data.name} message={Data.message} activity={Data.activity} time={Data.time} group={Data.group} isRead={Data.isRead}/>
-      })}
+      {item.map((Data, index)=>{
+  return <Notification images={Data.image} key={index} name={Data.name} message={Data.message} activity={Data.activity} time={Data.time} group={Data.group} isRead={Data.isRead} markAsRead={markAsRead} id={index}/>
+    })}
       </div>
   );
 }
--- src/Components/Notification/Notification.js
+++ src/Components/Notification/Notification.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./Notification.css";
 
-
-export default function Notification ({images, name, message, activity, time, group}){
- 
-    return(
-        <div className="container">
-            <img className="images" src={images}/>
+export default function Notification ({images, name, message, activity, time, group, isRead, markAsRead, id}){
+   return( 
+        <div className="container" onClick={()=>console.log(markAsRead(id))} style={{backgroundColor: isRead ? "transparent":"rgb(242, 246, 250)" }} >
+            <img className="images" src={images} alt="Avatar"/>
             <div>
-                <p><span className="name">{name}</span><span className="message">{message}</span><span className="activiti">{activity}</span><span className="group">{group}</span></p>
+                <p><span className="name">{name}</span><span className="message">{message}</span><span className="activiti">{activity}</span><span className="group">{group}</span> <span className="group">{isRead}</span></p>
                 <p className="time">{time}</p>
             </div>
         </div> 
-  
+
     )
 }
